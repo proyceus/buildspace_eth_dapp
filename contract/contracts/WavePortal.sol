2299f04f78f5b0e2785e9b0036a6049a3aa82c81
proyceus <proyceus@gmail.com>
2021-11-22 16:31:11 -0500

    finished writing first tests

--- contract/contracts/WavePortal.sol
+++ contract/contracts/WavePortal.sol
@@ -5,7 +5,22 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    mapping(address => uint256) public specificWaves;
+
+    function wave() public {
+        totalWaves += 1;
+        specificWaves[msg.sender] = specificWaves[msg.sender] += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+    
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
